ada48cd7e9c22bb95660b2bbace7996076bcc8cf
pedrofreitasmt <pedrohenrique_of@hotmail.com>
2026-09-14 16:01:52 -0400

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@teste.com',
+            'password' => Hash::make('123456'),
+        ]);
+    }
+}
